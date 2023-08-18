c1d1f1bf6447ea1a36f8663372bacd036d70d321
Vismay Igur <igur.vismay@gmail.com>
2023-08-18 17:50:24 -0400

    Created pokemon card component and main page.

--- assessment/src/card.jsx
+++ assessment/src/card.jsx
@@ -1,13 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import './card.css'
 
-/*
-This component is intended to be used for the pokemon card.
-We strongly recommend using this component.
-*/
-const Card = (props) => {
-    return (
-        <div></div>
-    )
+function Card({ pokemonName, pokemonHp, pokemonMoves, imageUrl }) {
+  return (
+    <div className="pokemon-card-container">
+        <div className="pokemon-card">
+            <div className="pokemon-header">
+                <h2 className="pokemon-name">{pokemonName}</h2>
+                <span className="pokemon-hp">HP: {pokemonHp}</span>
+            </div>
+            <div className="pokemon-image-container">
+                <img className="pokemon-image" src={imageUrl} alt={pokemonName} />
+            </div>
+            <div className="pokemon-details">
+                <div className="pokemon-moves">
+                    <p><strong>Moves:</strong></p>
+                    <ul>
+                        {pokemonMoves.map((move, index) => (
+                            <li key={index}>{move}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
 }
+
+Card.propTypes = {
+  pokemonName: PropTypes.string.isRequired,
+  pokemonHp: PropTypes.string.isRequired,
+  pokemonMoves: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+};
 
 export default Card
--- assessment/src/pokedex.jsx
+++ assessment/src/pokedex.jsx
@@ -1,17 +1,50 @@
+import React, { useState } from 'react';
 import pokemon from './pokemon.jsx'
 import './pokedex.css'
+import Card from './card.jsx';
 
 /* 
 This is the component for the pokedex page. 
 Feel free to edit anything in this file except the name of the component.
 */
-const Pokedex = (props) => {
-    return (
-        <div> 
-            <h1>Pokedex</h1>
-        </div>
 
-    )
-}
+
+    const Pokedex = (props) => {
+        const [selectedPokemon, setSelectedPokemon] = useState(null);
+
+        const handleButtonClick = (pokemon) => {
+            setSelectedPokemon(pokemon);
+        };
+
+        return (
+            <div> 
+                <div class="red-banner" id="redBanner">
+                    <p>Pokédex</p>
+                </div>
+                <div class="black-banner" id="blackBanner"></div>
+                <div class="flex-container">
+                    {pokemon.map((pokemon) => (
+                        <button
+                            key={pokemon.id}
+                            class="button"
+                            onClick={() => handleButtonClick(pokemon)}>
+                                {pokemon.name}
+                        </button>
+                    ))}
+                </div>
+
+                {selectedPokemon ? (
+                    <Card
+                        pokemonName={selectedPokemon.name}
+                        pokemonHp={selectedPokemon.hp}
+                        pokemonMoves={selectedPokemon.moves}
+                        imageUrl={selectedPokemon.image_src}
+                    />
+                ) : (
+                    <p class="select-text">Select a Pokémon!</p>
+                )}
+            </div>
+        );
+    }
 
 export default Pokedex
